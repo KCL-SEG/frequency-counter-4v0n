a152d1625b09177fc5e92e3ab9543b16d27700ab
4v0n <navodhk@hotmail.com>
2022-10-05 23:27:28 +0100

    Return frequencies of characters

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,15 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for character in items:
+
+        char = str(character)
+
+        if char in frequencies:
+            frequencies[char] = frequencies[char] + 1
+        
+        else:
+            frequencies[char] = 1
+
     return frequencies
